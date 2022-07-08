6768085402614a50b953d2311f140f2c29e614ac
michael <michael@mac>
2022-07-08 09:36:53 +0200

    init

---
+++ src/test/java/com/net128/app/spring/controller/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net128.app.spring.controller.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
