65414dfe829c75fa10d04ae323a68d3eb01e5481
michael <michael@mac>
2022-07-09 22:44:51 +0200

    ...

--- src/main/java/com/net128/app/spring/controller/test/controller/admin/AdminController.java
+++ src/main/java/com/net128/app/spring/controller/test/controller/admin/AdminController.java
@@ -1,8 +1,9 @@
 package com.net128.app.spring.controller.test.controller.admin;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
