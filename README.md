92cee8fb0d1ec17e74efbf04828b556ad4da767b
michael <michael@mac>
2022-07-10 12:33:32 +0200

    ...

---
+++ README.md
@@ -0,0 +1,18 @@
+# spring-controller-tests
+
+## Run
+```
+./gradlew bootRun
+```
+
+## Example calls
+```
+# no context attributes required
+curl "http://localhost:8080?context=%7B%22value%22:%20123%7D"
+
+# provide required context attributes: value1, value2
+curl -H "context-attributes: value1, value2" "http://localhost:8080/?context=%7B%20%22value1%22%3A%20123%2C%20%22value2%22%3A%20456%20%7D"
+
+# fail to to provide required context attributes: value1, value2
+curl -H "context-attributes: value1, value2" "http://localhost:8080/?context=%7B%20%22value0%22%3A%20123%2C%20%22value2%22%3A%20456%20%7D"
+```
--- src/main/java/com/net128/app/spring/controller/test/controller/main/Controller.java
+++ src/main/java/com/net128/app/spring/controller/test/controller/main/Controller.java
@@ -3,23 +3,45 @@ package com.net128.app.spring.controller.test.controller.main;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ValidationException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("unused")
 @RestController
 @Slf4j
 public class Controller {
 	final static ObjectMapper om = new ObjectMapper();
 
 	@GetMapping("/")
-	public String index(@RequestParam(value = "context", required = false) String context) {
+	public ResponseEntity<String> index(
+		@RequestParam(value = "context", defaultValue = "{}") String context,
+		@RequestHeader(value = "context-attributes", defaultValue = "") String contextAttributes
+	) {
 		try {
 			var typeRef = new TypeReference<HashMap<String, String>>() {};
-			return "Context: " + om.readValue(context, typeRef);
+			var contextMap = om.readValue(context, typeRef);
+			if(!StringUtils.isAllBlank(contextAttributes))  {
+				var contextAttributeList = new ArrayList<>(List.of(
+					contextAttributes.replaceAll("\\s", "").split(",")));
+				contextAttributeList.removeAll(contextMap.keySet());
+				if(contextAttributeList.size()>0) {
+					return ResponseEntity.badRequest().body(
+						"The following attributes are missing from the context: "+
+						contextAttributeList.stream().map(String::valueOf)
+							.collect(Collectors.joining(", "))
+					);
+				}
+			}
+			return ResponseEntity.ok("Context: " + contextMap);
 		} catch(Exception e) {
 			throw new ValidationException("Failed to validate context", e);
 		}
